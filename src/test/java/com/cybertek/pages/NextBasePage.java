f0541d6f6e714ae17fe7e98c4a840d3f669273a3
Emin Oruj <eminorujov86@gmail.com>
2021-07-23 00:38:28 -0400

    emins-first commit

--- src/test/java/com/cybertek/pages/NextBasePage.java
+++ src/test/java/com/cybertek/pages/NextBasePage.java
@@ -3,6 +3,8 @@ package com.cybertek.pages;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.concurrent.TimeUnit;
@@ -11,4 +13,30 @@ public class NextBasePage {
     public NextBasePage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(name = "USER_LOGIN")
+    public WebElement login;
+
+    @FindBy (name = "USER_PASSWORD")
+    public WebElement password;
+
+    @FindBy (id = "USER_REMEMBER")
+    public WebElement rememberbutton;
+
+    @FindBy (xpath = "//a[@class='login-link-forgot-pass']")
+    public WebElement forgotpasswor;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//span[@class='user-name']")
+    public WebElement profileButton;
+
+    @FindBy(xpath = "//span[.='Log out']")
+    public WebElement logOutButton;
+
+    @FindBy(xpath = "//input[@name='USER_REMEMBER']")
+    public WebElement checkRemember;
+
+    @FindBy(xpath = "//a[@class='login-link-forgot-pass']")
+    public WebElement forgotPassLink;
 }
--- src/test/java/com/cybertek/step_definitions/NextBase_StepDefinitions.java
+++
@@ -1,5 +0,0 @@
-package com.cybertek.step_definitions;
-
-public class NextBase_StepDefinitions {
-
-}
